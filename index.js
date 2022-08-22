aaa62b77c6b611e7098db6705e7a4fc66fa8d0a7
kelicaleb <kelicaleb7@gmail.com>
2022-08-22 15:23:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(rollerSkate = "This Saturday, I want to roller-skate!"){
+    console.log(rollerSkate)
+}
+saturdayFun("This Saturday, I want to bathe my dog!");
+const mondayWork = function(goToTheOffice = "This Monday, I will go to the office."){
+    console.log(goToTheOffice);
+}
+mondayWork("This Monday, I will work from home.");
+
+function wrapAdjective(visual = '*'){
+    return function(parameter = "special"){
+        let name = "You are ";
+        console.log(visual + name + parameter + visual)
+    }
+}
+let result = wrapAdjective()("a hardworker");
+let result2 = wrapAdjective("||")("a dedicated programmer");
